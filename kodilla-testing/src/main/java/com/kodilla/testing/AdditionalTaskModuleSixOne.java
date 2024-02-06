24eec8a9313aaf91341f12986f70fff303d7e47b
Marta Kowalczyk <martusia@mat.umk.pl>
2024-02-06 22:59:01 +0100

    6.1 Solution of additional task added - Vowels

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/AdditionalTaskModuleSixOne.java
@@ -0,0 +1,27 @@
+//Napisz program, który z podanego ciągu znaków znajdzie same samogłoski.
+//Na przykład dla napisu “Adam” zwróci tylko “Aa”.
+//Kluczowy algorytm zamknij w osobnej funkcji/metodzie, która będzie przyjmowała 1 argument wejściowy.
+//Funkcja ta nie powinna wypisać niczego na ekran.
+package com.kodilla.testing;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class AdditionalTaskModuleSixOne {
+
+    private final static Set<Character> vowelsSet = new HashSet<>(List.of('a', 'e', 'i', 'o', 'u'));
+
+    public static StringBuilder getVowels(String s) {
+        StringBuilder sWithVowelsOnly = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (vowelsSet.contains(s.toLowerCase().charAt(i))) {
+                sWithVowelsOnly.append(s.charAt(i));
+            }
+        }
+
+        return sWithVowelsOnly;
+    }
+}
+
--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -3,5 +3,7 @@ package com.kodilla.testing;
 public class TestingMain {
     public static void main(String[] args) {
         System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+
+        System.out.println(AdditionalTaskModuleSixOne.getVowels("Adam"));
     }
 }
