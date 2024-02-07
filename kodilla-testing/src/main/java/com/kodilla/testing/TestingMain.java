81db17f4a0a05fb5538ba97468ac1a9986828db9
Marta Kowalczyk <martusia@mat.umk.pl>
2024-02-07 19:37:27 +0100

    6.2 First unit test added

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,9 +1,37 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
         System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
-
+// 6.1 Additional task - vowels
         System.out.println(AdditionalTaskModuleSixOne.getVowels("Adam"));
+// 6.2 Example - unit test
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUserName();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("ERROR");
+        }
+// 6.2 First unit test
+        int a = 0;
+        int b = 10;
+
+        if (Calculator.add(a, b) == a + b) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("ERROR");
+        }
+
+        if (Calculator.subtract(a, b) == a - b) {
+            System.out.println("Test OK");
+        } else  {
+            System.out.println("ERROR");
+        }
     }
 }
